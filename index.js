c3f7779b83f44a81c1b4bc16dc92bef9f7b347ee
Olayinka Samuel <samuelolayinka2760.com>
2024-06-05 13:02:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+///////////NODE JS/////////////////////
+
+// const http = require("http");
+
+// const port = 4000;
+
+// const server = http.createServer((req, res) => {
+//   console.log(req);
+//   res.write("Welcome to backend");
+//   res.end();
+// });
+
+// server.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+//////////////// EXPRESSS JS///////////////
+
+// import express from "express";
+
+// const app = express();
+
+// const port = 4000;
+
+// app.get("/", (req, res) => {
+//   console.log(req);
+//   res.send("Welcome to backend");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+///////////// REACT JS /////////////////
+import Cars from "../Backend/Data.js";
+import express from "express";
+
+const app = express();
+
+const allData = [...Cars];
+const port = 4000;
+
+app.get("/", (req, res) => {
+  // console.log(req);
+  res.send(allData);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
